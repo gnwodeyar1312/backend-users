b05bc246a58b4498545b9740c36cd9ed02412e9d
gurunanje wodeyar <gnwodeyar@gmail.com>
2024-09-07 09:15:27 +0530

    feat: added sequelize and all other requirerd packages and modified code

--- index.js
+++ index.js
@@ -1,65 +1,48 @@
+require('dotenv').config();
 const express = require("express");
+const sequelize = require('./lib/sequelize.js');
+const userModel = require('./models/user.js');
 const cors = require("cors");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const users = [
-    {
-      id: 1,
-      username: 'octocat',
-      name: 'The Octocat',
-      repoCount: 8,
-      location: 'San Francisco',
-    },
-    {
-      id: 2,
-      username: 'torvalds',
-      name: 'Linus Torvalds',
-      repoCount: 25,
-      location: 'Portland',
-    },
-    {
-      id: 3,
-      username: 'gaearon',
-      name: 'Dan Abramov',
-      repoCount: 50,
-      location: 'London',
-    },
-    {
-      id: 4,
-      username: 'addyosmani',
-      name: 'Addy Osmani',
-      repoCount: 42,
-      location: 'Mountain View',
-    },
-    {
-      id: 5,
-      username: 'tj',
-      name: 'TJ Holowaychuk',
-      repoCount: 150,
-      location: 'Victoria',
-    },
-  ];
+sequelize.sync().then(() => {
+  console.log("Database Connected and Synced");
+}).catch(() => {
+  console.error("Unable to Connect")
+})
+
+
 
 //Exercise 2: Get all users
-app.get("/users", (req, res) => {
+app.get("/users", async (req, res) => {
+  try {
+    users = await userModel.findAll();
     res.json({ users });
+  } catch (error) {
+    return res.status(500).json({ error: "Unable to fetch Users"})
+  }
 });
 
 //Exercise 3: Get user by ID
-app.get("/users/:id", (req, res) => {
+app.get("/users/:id", async (req, res) => {
+  try {
     let id = parseInt(req.params.id);
-    let user = users.find((user) => user.id === id);
-    if(user) {
-        return res.json({ user})
+    let user = await userModel.findByPk(id);
+    if (user) {
+      return res.json({ user })
     } else {
-        return res.status(404).json({message: "No users found for " + id});
+      return res.status(404).json({ message: "No users found for " + id });
     }
+  } catch (error) {
+    return res.status(500).json({ error: "Unable to fetch User by Id"})
+  }
+
 })
 
 const PORT = 3000;
-app.listen(PORT, () =>{
-    console.log(`Server is running on port http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
 })
